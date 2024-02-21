58f95c010fe23f4eeb106503262960c1749b7e8f
Lara Santos <larasantos@MacBook-Pro.broadband>
2024-02-21 20:27:41 +0000

    Add first components of the deck of cards library

---
+++ src/Card.ts
@@ -0,0 +1,18 @@
+import {Suit} from './Suit';
+import {Rank} from './Rank';
+
+class Card {
+        //Properties
+    public rank: Rank;
+    public suit: Suit;
+
+    //Constructor to initialize the properties
+    constructor(rank: Rank, suit: Suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+    //Method to display the card
+    displayCards() {
+        return //this card of //this suit
+    }
+}
